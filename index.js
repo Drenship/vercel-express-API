daa9037795956df449a261e1f642e67f084193b0
Drenship <florentin.greneche@gmail.com>
2022-07-17 20:38:51 +0200

    update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from "express";
 const app = express();
 const port = 9000;
 
+app.use(require('./lib/router/router'));
 
 app.use("/", (req, res) => {
   res.json({ message: "Hello From Express App" });
