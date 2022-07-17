e92b90281f75967663b68d10fa99a6bdec0ba328
Drenship <florentin.greneche@gmail.com>
2022-07-17 21:11:15 +0200

    update

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 
 const app = express();
 const port = 9000;
 
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 
 app.use(require('./lib/router/router'));
 
